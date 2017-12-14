af6ab085b6d0ca868169404938905f484df8579a
rcordial <r.cordial@irri.org>
2017-12-14 12:04:59 +0800

    Modify honeywell js

--- www/honeywell.js
+++ www/honeywell.js
@@ -1,9 +1,17 @@
 var exec = require('cordova/exec');
 
-var honeywell = {
-    listenForScans: function(success, failure) {
-        return exec(success, failure, "HoneywellScannerPlugin", "listenForScans", []);
-    }
-};
 
-module.exports = honeywell;
+
+function honeywellScanner() {
+  console.log("honeywell.js: is created");
+ }
+
+honeywellScanner.prototype.listenForScans = function(success, failure) {
+  return exec(success, failure, "HoneywellScannerPlugin", "listenForScans", []);
+}
+
+var honeywell = new honeywellScanner();
+
+
+module.exports = honeywellScanner;
+
